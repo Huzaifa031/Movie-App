0d9592616639ac09becedcb2668d34d8afbdd5cd
Your Name <you@example.com>
2024-08-19 21:44:43 +0500

    first commit

---
+++ src/components/MovieListheading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieListheading = (props)=>{
+    return(
+        <div className="col">
+            <h1>{props.heading}</h1>
+        </div>
+    )
+}
+
+export default MovieListheading;
